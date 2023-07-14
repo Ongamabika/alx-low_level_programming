552a327a2c41a725f917ac3ea08488fd1328d83d
Ongamabika <bikaongama@gamil.com>
2023-07-14 16:15:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
+
 /**
- * main - A program that prints a line with puts function
- * Return : 0
+ * main - prints "Programming is like building a multiligual
+ * puzzle, follwed by a newline.
+ * Return: always 0.
  */
+
 int main(void)
+
 {
-puts ("\"Programming is like building a multilingual puzzle");
-Return (0);
+	puts("\"Programming is like building a multiligual puzzle");
+	return(0);
 }
