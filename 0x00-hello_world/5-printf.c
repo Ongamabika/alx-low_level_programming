0ecd0dcf9b28e323a8d0a7252bcad08980956297
Ongamabika <bikaongama@gamil.com>
2023-07-17 04:21:32 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,15 @@
 #include <stdio>
 
+
 /**
  * main - prints "with proper grammer, but the outcome is
- * a piece of art,"followed newline
+ * a piece of art," followed newline
  * Return: always 0.
  *
  */
 
 int main(void)
+
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
