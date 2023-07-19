4655102555021c4c78b37ea14ff9fab25c1effd2
Ongamabika <bikaongama@gamil.com>
2023-07-19 13:55:10 -0700

    0-putchar.c

---
+++ main.h
@@ -0,0 +1,25 @@
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alpjabet_x10(void);
+
+int _islower(int c);
+
+int _islalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jact_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
